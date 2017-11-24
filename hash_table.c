6ca59d76261218e6bb27c6afe2da5378a6ad0eb9
poncgo <102172350@qq.com>
2017-11-24 23:46:44 +0800

    rehash

--- hash_table.c
+++ hash_table.c
@@ -29,7 +29,7 @@ static inline void INIT_HASH_TB(hash_tb *tb, unsigned int size)
     tb->table = (struct hlist_head *)malloc(size * HLIST_SIZE);
     if (NULL == tb->table)
     {
-        printf("table mem initial failed\n");
+        printf("%s, %d:table mem initial failed\n", __FILE__, __LINE__);
         return;
     }
     tb->size = size;
@@ -44,8 +44,7 @@ static inline void INIT_HASH(void)
 {
     s_hash.rehashindex = -1;
     INIT_HASH_TB(&s_hash.ht[0], HASH_TB_MIN_NUM);
-
-    INIT_HASH_TB(&s_hash.ht[1], HASH_TB_MIN_NUM * 2);    
+   
 }
 
 static inline void INIT_HASH_ENTITY(hash_entity *n, char *k, char *v)
@@ -81,51 +80,152 @@ unsigned int DJBHash(char *str)
     return hash;
 }
 
+
+/*
+ *
+ * 
+ *    
+*/
+static inline int rehash_step(struct hash_t *h)
+{
+    int rehash_num = 8;
+    int rehash_step = 3;
+    unsigned int idx = 0;
+    hash_entity *p_entity = NULL;
+    struct hlist_head *he = NULL;
+    struct hlist_node *node = NULL;
+
+    if (!is_rehashing(h)) return 0; // no rehash happen & return;
+
+    while (--rehash_step && (0 != h->ht[0].used))
+    {
+
+        while(h->ht[0].table[h->rehashindex].first == NULL)
+        {
+            h->rehashindex++;
+            if (--rehash_num == 0) return 1;
+        }
+        node = h->ht[0].table[h->rehashindex].first;
+        while(node)
+        {
+            p_entity = (hash_entity *)node;
+            idx = DJBHash(p_entity->key) % h->ht[1].size;
+            hlist_add_head(node, (h->ht[1].table + idx));
+            h->ht[0].used--;
+            h->ht[1].used++;
+        }
+        h->ht[0].table[h->rehashindex].first = NULL;
+        h->rehashindex++;
+    }
+
+    if (0 == h->ht[0].used)
+    {
+        free(h->ht[0].table);
+        h->ht[0] = h->ht[1];
+
+    }
+    return 0;
+}
+
+static inline int need_expand(struct hash_t *h)
+{
+    if (is_rehashing(h))
+    {
+        printf("%s, %d :hash table rehashing\n", __FILE__, __LINE__);
+        return -1;
+    }
+    if (h->ht[0].size >= HASH_TB_MAX_NUM)
+    {
+        printf("%s, %d:max number reached\n", __FILE__, __LINE__);
+        return -1;
+    }
+    
+    if ((h->ht[0].size * 0.9) <= h->ht[0].used) 
+    {
+        return 1;
+    }
+    return 0;
+}
+
+static inline int need_resize(struct hash_t *h)
+{
+    if (is_rehashing(h))
+    {
+        printf("%s, %d:hash table rehashing \n", __FILE__, __LINE__);
+        return -1;
+    }
+    if (h->ht[0].size <= HASH_TB_MIN_NUM)
+    {
+        printf("%s, %d: min num reached\n", __FILE__, __LINE__);
+        return -1;
+    }
+    if (h->ht[0].size * 0.1 < h->ht[0].used)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+static inline int hash_expand(struct hash_t *h, unsigned int new_size)
+{
+    hash_tb      t;
+    t.size = new_size;
+    t.table = (struct hlist_head *)malloc(new_size * HLIST_SIZE);
+    if (NULL == t.table)
+    {
+        printf("mem alloc failed\n");
+        return 0;
+    }
+
+    t.used = 0;
+    h->ht[1] = t;
+    h->rehashindex = 0;
+    return 1;
+}
 /*
     get the idx of hash table by mode tabel size
 */
-static inline unsigned int get_hash_idx(char * key, struct hash_t *hash)
+static inline int get_hash_idx(char * key, unsigned int hash, struct hash_t *h)
 {
+    unsigned int idx, table_index;
+    struct hlist_node *he = NULL;
+    hash_entity *p_entity = NULL;
 
-    return -1;
+    for (table_index = 0; table_index <2; ++table_index)
+    {
+        idx = hash % (h->ht[table_index].size);
+        he = h->ht[table_index].table[idx].first;
+        while (he)
+        {
+            p_entity = (hash_entity *)he;
+            if (0 == strcmp(key, p_entity->key))
+            {
+                printf("key:%s found in table %d\n", key, table_index);
+                return -1;
+            }
+            he = he->next;
+        }
+        if (!is_rehashing(h))
+        {
+            break; //just on table 0, and -1 to return. also no index ;
+        }
+    }
+    return idx;
 }
 
 
 /*
    judge if a key exist, and return hash_table index, else -1
 */
-static inline hash_entity *hash_is_exist(char *key, struct hash_t *hash)
+static inline int hash_is_exist(char *key, struct hash_t *hash)
 {
-
-    if (NULL == key)
-    {
-        printf( "key is  null\n");
-        return NULL;
-    }
-
-    unsigned int idx      = get_hash_idx(key, &s_hash);
-    struct hlist_node *he = ht.hash_table[idx].first;
-    hash_entity *p_entity = NULL;
-
-    while(he)
-    {
-        p_entity = (hash_entity *) he;
-        if (0 == strcmp(p_entity->key, key))
-        {
-            return p_entity;
-        }
-        he = he->next;
-    }
-    return NULL;
+    return 0;
 }
 
-static inline void _RehashStep()
-{
-
-}
 int hash_add(char *key, char *val)
 {
     int idx = 0;
+    unsigned int hash = 0;
     struct hlist_head *he = NULL;
     hash_tb *tb = NULL;
     int l_k = strlen(key);
@@ -139,18 +239,24 @@ int hash_add(char *key, char *val)
         printf( "NULL POINTER of key or val\n");
         return -1;
     }
-    if (is_rehashing(&s_hash)) 
-    {
+        
+    if (is_rehashing(&s_hash)) rehash_step(&s_hash);
 
-    }
-       
-    if ( 0 > (idx = get_hash_idx(key, &s_hash)))
+    if  ((1 == need_expand(&s_hash)))
+    {
+        unsigned int new = (s_hash.ht[0].size) << 1;
+        hash_expand(&s_hash, new);
+    } 
+
+    hash = DJBHash(key);
+
+    if ( 0 > (idx = get_hash_idx(key,hash, &s_hash)))
     {
         printf( "idx get failed\ns");
         return -1;
     }
-    tb =  is_rehashing(&s_hash) ? (s_hash.ht + 1) : (s_hash.ht); 
-
+    // if rehashing ,new entity will put to new table [1]. else table[0] will be choosed.
+    tb = is_rehashing(&s_hash) ? (s_hash.ht + 1) : (s_hash.ht); 
     p_entity = malloc(sizeof(hash_entity));
     k = malloc(l_k * sizeof(char));
     v = malloc(l_v * sizeof(char));
@@ -159,39 +265,42 @@ int hash_add(char *key, char *val)
     strcpy(v, val);
     
     INIT_HASH_ENTITY(p_entity, k, v);
-    he = (ht.hash_table + idx);
+    he = (tb->table + idx);
     hlist_add_head(&(p_entity->node), he);
-    ht.used++;
+    tb->used++;
     return idx;
 }
 
 
 int hash_del(char *key, char *val)
 {
-    int idx = hash_idx(key);
+
+    int idx = 0;
+    unsigned int hash = 0;
+    int table_index = 0;
     hash_entity *p_entity = NULL;
     struct hlist_node *node = NULL;
-    if (0 > idx)
+    
+    if (is_rehashing(&s_hash)) rehash_step(&s_hash);
+
+    if (need_resize(&s_hash)) 
     {
-        printf( "key error\n");
-        return -1;
+        unsigned int new = (s_hash.ht[0].size) >> 1;
+        hash_expand(&s_hash, new);
+    }
+    
+    hash = DJBHash(key);
+
+    for (table_index = 0; table_index < 2; ++table_index)
+    {
+        idx = hash % s_hash.ht[0].size;
+        if ()
+
+
+
     }
 
-    p_entity = hash_is_exist(key);
-    if (NULL == p_entity)
-    {
-        printf( "no key found while del\n");
-        return -1;
-    }
 
-    node = &(p_entity->node);
-    hlist_del(node);
-    ht.used--;
-
-    if (NULL != val)
-    {
-        strcpy(val, p_entity->val);
-    }
 
     free(p_entity->key);
     free(p_entity->val);
--- hash_table.h
+++ hash_table.h
@@ -3,9 +3,11 @@
 #include <limits.h>
 
 #include "list.h"
+
+
 #define HASH_TB_MIN_NUM 8
 
-#define HASH_TB_MAX_NUM INT_MAX
+#define HASH_TB_MAX_NUM 0x8000
 
 #define HLIST_SIZE sizeof(struct hlist_head)
 
@@ -18,19 +20,37 @@
 
 #define VAL_LEN 40
 
+/*
+    @struct hlist_head *table :hash table.
+    @unsigned int size : current size of table;
+    @unsigned int used : number of entities in the table
+*/
 typedef struct hash_table_t
 {
-    struct hlist_head *table;
-    unsigned int size;
-    unsigned int used;
+    struct hlist_head *table; // table to store 
+    unsigned int size;        // size of hash table
+    unsigned int used;        // number of entity in table
 }hash_tb;
 
+/*
+    @hash_tb ht[2]: two hash_tb defined, [0] used for store entity as normal conditions,
+                    [1] used for store entity while rehashing such as expand or decrease.
+    @rehashindex :  normally, it equal to -1 , but when rehashing happened, it may the value of (0 ~ ht[0].size)
+                    to indicate current rehashing table index, index small than it has rehased into ht[0],
+                    use as ht[0].table[rehashingindex],
+*/
 struct hash_t
 {
-    hash_tb ht[2];
+    
+    hash_tb ht[2];           
     unsigned int rehashindex;
 }Hash;
 
+/*
+    @struct hlist_node: node to index the entity in table,
+    @char *key : key of entity 
+    @char *val : value of entity
+*/
 typedef struct hash_entity_t
 {
     struct hlist_node node; //use hlist_node to link entity while conflict, also by convenience of api of hlist.
@@ -38,13 +58,33 @@ typedef struct hash_entity_t
     char * val;
 }hash_entity;
 
+/*
+    @Function Name : hash_add
+    @param  inout :key ,key of val
+    @param  output:val  value to store
+    @return    : -1, when add vall failed if key exist, or integer >= 0
+    @Description : add  a string <key , val> into a hash table
+*/
 int hash_add(char *key, char *val);
 
+
+/*
+    @function name : hash_del
+    @param input :key, key of value to be delete
+    @param output :val , :value to be delete
+    @return : -1 if key not find, or integer >= 0
+    @
+    @description: delete a string from hash table
+*/
 int hash_del(char *key, char *val);
 
+/*
+    @function name : hash_find
+    @param   input: key of value to be find.
+    @param  output: value to find
+    @return        : -1 i
+*/
 int hash_find(char *key, char *val);
-
-int rehash_table(void);
 
 
 
